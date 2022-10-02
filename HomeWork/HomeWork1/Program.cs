bb1127196718f1d41a089b325fd81800953a718f
Igor <Chertopiatov@yandex.ru>
2022-10-02 18:12:23 +0300

    Second task

--- HomeWork/HomeWork1/Program.cs
+++ HomeWork/HomeWork1/Program.cs
@@ -21,3 +21,29 @@ else
 }
 */
 
+/*
+//Задача 4. Напишите программу которая принимает на вход три числа и выдает макчимаьное из этих чисел.
+
+Console.WriteLine ("Введите 1 число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите 2 число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите 3 число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int MaxNum = n1;
+
+if (n2 > MaxNum)
+{
+    MaxNum = n2;
+}
+
+if (n3 > MaxNum)
+{
+    MaxNum = n3;
+}
+
+Console.WriteLine ($"Максимальное число {MaxNum}");
+*/
