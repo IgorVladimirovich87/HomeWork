0d42245706e9928060fbae9596464e3377910be6
Igor <Chertopiatov@yandex.ru>
2022-10-06 13:04:48 +0300

    second task of second seminar

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -22,3 +22,23 @@ int biggestDigit = FindBiggestDigit(number);
 Console.WriteLine($"The biggest digit of {number} is {biggestDigit}");
 */
 
+// Напишите программу, которая выводит случайное 3-х значное число и удаляет вторую циифру этого числа.
+
+/*
+int TwoDigitNumber(int num)
+{
+    int result;
+
+    int ed = num % 10;
+    int dec = num / 100;
+
+    result = dec * 10 + ed;
+    return result;
+}
+
+int number =new Random().Next(100, 1000);
+int TwoDigit = TwoDigitNumber(number);
+
+Console.WriteLine($" чило из первой и последней цифры от {number} это {TwoDigit}");
+*/
+
