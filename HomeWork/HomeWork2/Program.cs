8d6996b60334f12e74b978f44943e812d856ea0e
Igor <Chertopiatov@yandex.ru>
2022-10-06 15:17:11 +0300

    first task HomeWork

---
+++ HomeWork/HomeWork2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Написать программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+
+int FindSecondDigit(int num)
+{   int result;
+
+    int number = num / 10 % 10;
+    
+    result = number; 
+    return result;
+}
+
+Console.Write ("Введите 3-х значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondDigit = FindSecondDigit(number);
+Console.WriteLine($"Второе число из {number} это {secondDigit}" );
