fb295863938cb0b03c195821eda2d6ecc3a6ba03
Igor <Chertopiatov@yandex.ru>
2022-10-06 16:24:44 +0300

    15 task HomeWork

--- HomeWork/HomeWork2/Program.cs
+++ HomeWork/HomeWork2/Program.cs
@@ -15,3 +15,23 @@ int secondDigit = FindSecondDigit(number);
 Console.WriteLine($"Второе число из {number} это {secondDigit}" );
 */
 
+// Задача 15. Программа, которая принимает на вход цифру, обозначающую день недели и проверяет, является ли этот день выходным.
+
+bool HolidayDays(int day)
+{
+    bool result;
+    if (day == 6 ^ day == 7)
+        result = true;
+    else
+        result = false;
+    return result;
+}
+
+Console.Write ("Введите число обозночающие день недели ");
+int dayNumber = Convert.ToInt32 (Console.ReadLine());
+bool isHoliday = HolidayDays(dayNumber);
+    if (isHoliday)
+        Console.WriteLine($"Да {dayNumber} день выходной ");
+    else
+        Console.WriteLine($"Нет {dayNumber} день не выходной");
+
