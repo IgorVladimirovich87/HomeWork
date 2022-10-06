9773cbdb70301fab72fc2efaf27a16c090c50db0
Igor <Chertopiatov@yandex.ru>
2022-10-06 10:58:26 +0300

    task one second seminar

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа.
+/*
+int FindBiggestDigit(int num)
+
+{
+    int max;
+
+    int ed = num % 10;
+    int dec = num / 10;
+
+    if(ed > dec)
+        max = ed;
+    else
+        max = dec;
+
+    return max;       
+}
+
+int number = new Random().Next(10,100);
+int biggestDigit = FindBiggestDigit(number);
+
+Console.WriteLine($"The biggest digit of {number} is {biggestDigit}");
+*/
+
