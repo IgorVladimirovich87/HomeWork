6cd143f713b5e5f9d22e587cd34fc9fa85dc1122
Igor <Chertopiatov@yandex.ru>
2022-10-10 17:58:16 +0300

    HomeWork task21 of Seminar 3

---
+++ HomeWork/HomeWork3/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
+double FindDistance(double ax, double ay, double az, double bx, double by, double bz)
+{
+    return Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+}
+Console.Write("Введите коордитану X точки A: ");
+double ax = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коордитану Y точки A: ");
+double ay = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коордитану Z точки A: ");
+double az = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Точка A будет иметь координаты ({ax}, {ay}, {az}).");
+Console.Write("Введите коордитану X точки B: ");
+double bx = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коордитану Y точки B: ");
+double by = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коордитану Z точки B: ");
+double bz = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Точка B будет иметь координаты ({bx}, {by}, {bz}).");
+Console.WriteLine($"Расстояние между точкой A({ax}, {ay}, {az}) и точкой B({bx}, {by}, {bz}) равно {Math.Round(FindDistance(ax, ay, az, bx, by, bz), 2)}.");
+*/
+
