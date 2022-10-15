123c3633454fb9a4e638b59c15d5a40c608ba6d6
Igor <Chertopiatov@yandex.ru>
2022-10-15 13:10:25 +0300

    HomeWork Seminar 4

---
+++ HomeWork/HomeWork4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+int APowB(int a, int b)
+{
+    int res = 1;
+    for (int i = 0; i < b; i++)
+        res *= a;
+    return res;
+}
+
+Console.WriteLine("Цикл, который возводит первое число в степень второго числа ");
+Console.Write("Введите первое число >: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число >: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{numA}^{numB} = {APowB(numA, numB)}");
+*/
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
