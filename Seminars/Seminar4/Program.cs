7bd41792989f787bc29323525a35d8ae53138165
Igor <Chertopiatov@yandex.ru>
2022-10-15 11:55:34 +0300

    Seminar4

--- Seminars/Seminar4/Program.cs
+++ Seminars/Seminar4/Program.cs
@@ -57,4 +57,27 @@ Console.WriteLine($"Произведение чисел от 1 до {num} рав
 */
 
 //Напишите программу, которая выводит массив из M элементов, заполненный нулями и единицами в случайном порядке.
+/*
+int [] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue +1);
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i]+ " ");
+
+    Console.WriteLine();
+}
+Console.Write("Input a number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int min = 0;
+int max = 1;
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
+*/
 
