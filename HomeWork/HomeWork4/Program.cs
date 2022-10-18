295f058ea177d9f4c6b646be0ef14506454a3fe4
Igor <Chertopiatov@yandex.ru>
2022-10-18 06:45:05 +0300

    HomeWork Seminar 4

--- HomeWork/HomeWork4/Program.cs
+++ HomeWork/HomeWork4/Program.cs
@@ -35,3 +35,45 @@ int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Сумма цифр числа {number} равна {SumOfDigits(number)}.");
 */
 
+
+// Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+/*
+int[] MakeIntArray(int size, int minValue, int maxValue) 
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintIntArray(int[] array)
+{
+    Console.Write("{ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+            Console.Write(", ");
+    }
+    Console.WriteLine(" }");
+}
+
+Console.Write("Введите размер массива >: ");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+    while (arrSize < 0)
+    {
+        Console.WriteLine("Размер массива не может быть отрицательным, введите неотрицательное число ");
+        arrSize = Convert.ToInt32(Console.ReadLine());
+    }
+
+Console.Write("Введите нижнюю границу для генерации чисел >: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите верхнюю границу для генерации чисел >: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+PrintIntArray(MakeIntArray(arrSize, minValue, maxValue));
+*/
