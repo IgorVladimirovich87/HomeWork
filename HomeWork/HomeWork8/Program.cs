76bca98b614fb4b7d450f5121cd99077dbb790c7
Igor <Chertopiatov@yandex.ru>
2022-11-09 15:01:06 +0300

    HomeWork8 all  task

--- HomeWork/HomeWork8/Program.cs
+++ HomeWork/HomeWork8/Program.cs
@@ -143,4 +143,191 @@ Console.WriteLine($"Номер строки с наименьшей суммой
 */
 
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+/*
+Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: ");
+
+int[,] firstMartrix = new int[m, n];
+int[,] secomdMartrix = new int[n, p];
+int[,] resultMatrix = new int[m,p];
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+
+{
+  for (int i = 0; i < array.GetLength(1); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+CreateArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+WriteArray(firstMartrix);
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+WriteArray(secomdMartrix);
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+WriteArray(resultMatrix);
+*/
+
+//Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+/*
+int[] TwoDigitPositiveNumsArray()
+{
+    int[] array = Enumerable.Range(10, 90).ToArray();
+    for (int i = 0; i < array.Length; i++)
+    {
+        int j = new Random().Next(0, 90);
+        if (j != i)
+        {
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    return array;
+}
+
+int[,,] Create3DArray(int[] array, int height, int width, int depth)
+{
+    int m = 0;
+    int[,,] array3D = new int[height, width, depth];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            for (int k = 0; k < depth; k++)
+            {
+                array3D[i, j, k] = array[m];
+                m++;
+            }
+        }
+    }
+    return array3D;
+}
+
+void Print3DArray(int[,,] array)
+{
+    Console.WriteLine("\nЭлементы с индексами (i,j,k) в трехмерном массиве: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k] + " ");
+                Console.Write($"({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите высоту (количество строк): ");
+int height = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите ширину(количество столбцов): ");
+int width = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите глубину массива: ");
+int depth = Convert.ToInt32(Console.ReadLine());
+int[] array = TwoDigitPositiveNumsArray();
+int[,,] array3D = Create3DArray(array, height, width, depth);
+Print3DArray(array3D);
+*/
+
+//Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+/*
+int[,] FillSquareArraySpiral(int rows, int columns)
+{
+    int[,] arraySpiral = new int[rows, columns];
+    for (int passage = 0, valueElements = 1; valueElements <= rows * columns; passage++)
+    {
+        for (int i = passage, j = passage; j < columns - passage; j++)
+        {
+            arraySpiral[i, j] = valueElements;
+            valueElements++;
+        }
+        if(valueElements > rows * columns) break;
+        for (int i = passage + 1, j = columns - passage - 1; i < rows - passage; i++)
+        {
+            arraySpiral[i, j] = valueElements;
+            valueElements++;
+        }
+        if(valueElements > rows * columns) break;
+        for (int i = rows - passage - 1, j = columns - passage - 2; j >= passage; j--)
+        {
+            arraySpiral[i, j] = valueElements;
+            valueElements++;
+        }
+        if(valueElements > rows * columns) break;
+        for (int i = rows - passage - 2, j = passage; i > passage; i--)
+        {
+            arraySpiral[i, j] = valueElements;
+            valueElements++;
+        }
+    }
+    return arraySpiral;
+}
+
+void PrintArraySpiral(int[,] array, int fillZeros = 2)
+{
+    Console.WriteLine("\nМассив, заполненный спирально: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j].ToString($"D{fillZeros}") + " "); 
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] arraySpiral = FillSquareArraySpiral(rows, columns);
+PrintArraySpiral(arraySpiral);
+*/
 
