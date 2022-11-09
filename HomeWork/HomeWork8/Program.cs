e6d2edfdf5bc93c73928d3801498f828440f9eff
Igor <Chertopiatov@yandex.ru>
2022-11-09 10:19:10 +0300

    HomeWork8 task 2

---
+++ HomeWork/HomeWork8/Program.cs
@@ -0,0 +1,146 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+/*
+int[,] Create2Array()
+{
+    Console.Write("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите возможный минимальный элемент массива: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите возможный максимальный элемент массива: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void Print2Array(int[,] array)
+{
+    Console.WriteLine("Созданный массив: ");
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeOrderElementsRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, j])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, j];
+                    array[i, j] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] newArray = Create2Array();
+Print2Array(newArray);
+Console.WriteLine();
+int[,] changeArray = ChangeOrderElementsRows(newArray);
+Print2Array(changeArray);
+*/
+
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+/*
+int[,] Create2Array()
+{
+    Console.Write("Введите количество строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите возможный минимальный элемент массива: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите возможный максимальный элемент массива: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void Print2Array(int[,] array)
+{
+    Console.WriteLine("Созданный массив: ");
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "; ");
+
+        Console.WriteLine();
+    }
+}
+
+int[] SumElementsRows(int[,] array)
+{
+    int[] sumArray = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i, j];
+            sumArray[i] = sum;
+        }
+    }
+    return sumArray;
+}
+
+void PrintSumArray(int[] array)
+{
+    Console.Write("Сумма элементов в строке: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
+
+int IndexMinElements(int[] array)
+{
+    int iMin = 0;
+    int min = array[iMin];
+    for (int i = 1; i < array.Length - 1; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+            iMin = i;
+        }
+    }
+    return iMin;
+}
+
+int[,] newArray = Create2Array();
+Print2Array(newArray);
+Console.WriteLine();
+int[] sumArray = SumElementsRows(newArray);
+PrintSumArray(sumArray);
+int numberRow = (IndexMinElements(sumArray) + 1);
+Console.WriteLine($"Номер строки с наименьшей суммой элементов - {numberRow}.");
+*/
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
